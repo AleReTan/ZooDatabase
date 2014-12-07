1fa5f202ea22011e994cac7a3825a5f03d518d8b
TAN93 <necrotan@gmail.com>
2014-12-07 17:39:07 +0300

    Some changes

---
+++ src/main/java/net/reshetnikov/Logic/Review.java
@@ -0,0 +1,66 @@
+package net.reshetnikov.Logic;
+
+import java.util.Date;
+
+/**
+ * Created by Александр on 13.11.2014.
+ */
+public class Review {
+
+    private int reviewNumber;
+    private String reviewFoodTitle;
+    private int reviewFoodCount;
+    private Date reviewDateOfDelivery;
+    private String reviewAimalTypeFood;
+
+    public int getReviewNumber() {
+        return reviewNumber;
+    }
+
+    public void setReviewNumber(int reviewNumber) {
+        this.reviewNumber = reviewNumber;
+    }
+
+    public String getReviewFoodTitle() {
+        return reviewFoodTitle;
+    }
+
+    public void setReviewFoodTitle(String reviewFoodTitle) {
+        this.reviewFoodTitle = reviewFoodTitle;
+    }
+
+    public int getReviewFoodCount() {
+        return reviewFoodCount;
+    }
+
+    public void setReviewFoodCount(int reviewFoodCount) {
+        this.reviewFoodCount = reviewFoodCount;
+    }
+
+    public Date getReviewDateOfDelivery() {
+        return reviewDateOfDelivery;
+    }
+
+    public void setReviewDateOfDelivery(Date reviewDateOfDelivery) {
+        this.reviewDateOfDelivery = reviewDateOfDelivery;
+    }
+
+    public String getReviewAimalTypeFood() {
+        return reviewAimalTypeFood;
+    }
+
+    public void setReviewAimalTypeFood(String reviewAimalTypeFood) {
+        this.reviewAimalTypeFood = reviewAimalTypeFood;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "reviewNumber=" + reviewNumber +
+                ", reviewFoodTitle='" + reviewFoodTitle + '\'' +
+                ", reviewFoodCount=" + reviewFoodCount +
+                ", reviewDateOfDelivery=" + reviewDateOfDelivery +
+                ", reviewAimalTypeFood='" + reviewAimalTypeFood + '\'' +
+                '}';
+    }
+}
